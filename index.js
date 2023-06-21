d01ba2fbe6d1172bda57670e6bdeff913a99f1e8
Wojciech Pieczonka <wojciechpieczonka@gmail.com>
2023-06-21 23:44:05 +0200

    totale revenue added

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const revInputNam = document.querySelector("#rev-name");
 
 let revInputNameE = " ";
-
-revInputNam.addEventListener("change", (event) => {
-  if (event.target.value > 0) {
-    alert("Podaj wartość większą od 0");
-  } else revInputNameE = event.target.value;
-});
+let expInputNameE = " ";
+let revSum = [];
+let expSum = [];
+let totalRev = 0;
+let totalExp = 0;
 
 const revInputAmo = document.querySelector("#rev-amo");
-let revInputAmoE = 1;
+let revInputAmoE = 0;
+
 revInputAmo.addEventListener("change", (event) => {
   if (event.target.value <= 0) {
     alert("Podaj wartość większą od 0");
@@ -17,25 +17,34 @@ revInputAmo.addEventListener("change", (event) => {
   } else revInputAmoE = event.target.value;
 });
 
-const AddRevBtn = document.querySelector("#rev-add");
+const addRevBtn = document.querySelector("#rev-add");
 
-AddRevBtn.addEventListener("click", () => {
+addRevBtn.addEventListener("click", () => {
   const listItem = document.createElement("li");
   listItem.setAttribute("contenteditable", "false");
   listItem.classList.add("addedLi");
+  revSum.push(Number(revInputAmoE));
+  totalRev = revSumCount(revSum);
+  const totalRevElement = document.querySelector("#r");
+  totalRevElement.textContent = "Total revenue: " + totalRev + " zł";
+  balance();
 
   const revItem = document.createElement("span");
-  revItem.innerHTML = revInputNameE + " - " + revInputAmo.value;
+  revItem.innerHTML = revInputNam.value + " - " + revInputAmo.value + " zł";
 
   const editBtn = document.createElement("button");
   editBtn.textContent = "Edit";
+  editBtn.classList.add("edit-btn");
+  editBtn.id = "edit-btn";
 
   const deleteBtn = document.createElement("button");
   deleteBtn.textContent = "Delete";
+  deleteBtn.classList.add("delete-btn");
+  deleteBtn.id = "delete-btn";
 
   const buttonContainer = document.createElement("span");
+  buttonContainer.id = "btnContainer";
   buttonContainer.appendChild(editBtn);
-  buttonContainer.appendChild(document.createTextNode(" ")); // Dodanie odstępu między przyciskami
   buttonContainer.appendChild(deleteBtn);
 
   listItem.appendChild(revItem);
@@ -55,27 +64,55 @@ AddRevBtn.addEventListener("click", () => {
   });
 });
 
-const expInputNam = document.querySelector("#exp-name");
-const expInputAmo = document.querySelector("#exp-amo");
+const revSumCount = () => {
+  let sum = revSum.reduce((accumulator, currentvalue) => {
+    return accumulator + currentvalue;
+  }, 0);
+  return sum;
+};
 
-const AddExpBtn = document.querySelector("#exp-add");
-AddExpBtn.addEventListener("click", () => {
+//Expenses//
+
+const expInputNam = document.querySelector("#exp-name");
+
+const expInputAmo = document.querySelector("#exp-amo");
+let sumInputAmoE = 0;
+
+expInputAmo.addEventListener("change", (event) => {
+  if (event.target.value <= 0) {
+    alert("Podaj wartość większą od 0");
+    event.target.value = 0;
+  } else {
+    sumInputAmoE = event.target.value;
+  }
+});
+
+const addExpBtn = document.querySelector("#exp-add");
+
+addExpBtn.addEventListener("click", () => {
   const listItem = document.createElement("li");
   listItem.setAttribute("contenteditable", "false");
   listItem.classList.add("addedLi");
 
-  const expItem = document.createElement("span");
-  expItem.innerHTML = expInputNam.value + " - " + expInputAmo.value;
+  expSum.push(Number(sumInputAmoE));
+  totalExp = expSumCount(expSum);
+  const totalExpElement = document.querySelector("#e");
+  totalExpElement.textContent = "Total expenses: " + totalExp + " zł";
+  balance();
+
+  const expItem = document.createElement("span"); /////////////////
+  expItem.innerHTML = expInputNam.value + " - " + expInputAmo.value + " zł";
 
   const editBtn = document.createElement("button");
   editBtn.textContent = "Edit";
+  editBtn.classList.add("edit-btn");
 
   const deleteBtn = document.createElement("button");
   deleteBtn.textContent = "Delete";
+  deleteBtn.classList.add("delete-btn");
 
   const buttonContainer = document.createElement("span");
   buttonContainer.appendChild(editBtn);
-  buttonContainer.appendChild(document.createTextNode(" ")); // Dodanie odstępu między przyciskami
   buttonContainer.appendChild(deleteBtn);
 
   listItem.appendChild(expItem);
@@ -94,3 +131,37 @@ AddExpBtn.addEventListener("click", () => {
     ul.removeChild(listItem);
   });
 });
+const expSumCount = () => {
+  let sum = expSum.reduce((accumulator, currentvalue) => {
+    return accumulator + currentvalue;
+  }, 0);
+  return sum;
+};
+
+const balance = () => {
+  const summary = document.querySelector(".summary");
+  summary.innerHTML = "";
+  if (totalRev === totalExp) {
+    const balanceZero = document.createElement("h2");
+    balanceZero.textContent = "The balance is 0 zł";
+    const zero = document.querySelector(".summary");
+    zero.appendChild(balanceZero);
+  } else if (totalRev > totalExp) {
+    const remainingAmount = totalRev - totalExp;
+    const balancePositive = document.createElement("h2");
+    balancePositive.textContent =
+      "You can still spend " + remainingAmount + " zł";
+    const positive = document.querySelector(".summary");
+    positive.appendChild(balancePositive);
+  } else if (totalRev < totalExp) {
+    const remainingAmount = totalExp - totalRev;
+    const balanceNegative = document.createElement("h2");
+    balanceNegative.textContent =
+      "The balance is negative. You're at a disadvantage " +
+      remainingAmount +
+      " zł";
+    const negative = document.querySelector(".summary");
+    negative.appendChild(balanceNegative);
+  }
+};
+balance();
